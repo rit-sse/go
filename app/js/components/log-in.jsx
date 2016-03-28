90a48b8fa291f55e983bbf50dbe69caf63198d81
Michael Timbrook <timbrook480@gmail.com>
2016-03-28 18:29:28 -0400

    fetch id

--- app/js/components/log-in.jsx
+++ app/js/components/log-in.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import SSEStore from '../stores/sse';
+import API from '../api';
 
 export default class LogIn extends React.Component {
   constructor() {
@@ -9,11 +10,18 @@ export default class LogIn extends React.Component {
 
   componentDidMount() {
     // Retrieve the singleton for the GoogleAuth library and set up the client.
-    const auth2 = gapi.auth2.init({
-      client_id: process.env.GOOGLE_CLIENT_ID, // eslint-disable-line camelcase
-      cookie_policy: 'single_host_origin', // eslint-disable-line camelcase
-    });
-    auth2.attachClickHandler(this.refs.button.getDOMNode(), {}, SSEStore.signIn );
+    API
+      .Auth
+      .clientId()
+      .then( ({ token }) => {
+        // Retrieve the singleton for the GoogleAuth library and set up the client.
+        const auth2 = gapi.auth2.init({
+          client_id: token, // eslint-disable-line camelcase
+          cookie_policy: 'single_host_origin', // eslint-disable-line camelcase
+        });
+        auth2.attachClickHandler(this.refs.button.getDOMNode(), {}, SSEStore.signIn );
+      }) 
+      .catch( err => console.log(err) );
   }
 
   render() {
